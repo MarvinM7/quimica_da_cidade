0377c4c573150ec51f3305cbfb278a3d804061c8
Marvin Miguel <marvin.silva@n3urons.com>
2024-02-20 20:24:14 -0300

    fix: removendo erro de tipo da position

--- components/Map.tsx
+++ components/Map.tsx
@@ -9,28 +9,14 @@ import "leaflet-defaulticon-compatibility";
 // END: Preserve spaces to avoid auto-sorting
 import { FeatureGroup, MapContainer, Marker, Polygon, Popup, TileLayer } from "react-leaflet";
 
+import { GOLF_CLUB, UFPE } from "@/constants/Places";
+
+
 export default function Map() {
   const center = {
     lat: -8.052770867957172,
     lng: -34.95059967041016,
   }
-
-  const UFPE = [
-    [-8.04639699140255, -34.953775405883796],
-    [-8.04656696274523, -34.94493484497071],
-    [-8.052367192083407, -34.94624376296998],
-    [-8.059782016298445, -34.94596481323243],
-    [-8.056382686813597, -34.95660781860352],
-  ];
-
-  const GOLF_CLUB = [
-    [-8.033181501402352, -34.952273368835456],
-    [-8.031141773286445, -34.95145797729493],
-    [-8.028507109306654, -34.95124340057374],
-    [-8.0278696880344, -34.94497776031495],
-    [-8.03441383217304, -34.94463443756104],
-    [-8.037048457822186, -34.94566440582276],
-  ];
 
   function DraggableMarker() {
     const [draggable, setDraggable] = useState(true);
@@ -86,12 +72,6 @@ export default function Map() {
         <Popup>Golf Club - Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</Popup>
         <Polygon positions={GOLF_CLUB} />
       </FeatureGroup>
-      {/* <Polygon pathOptions={{color: 'blue'}} positions={polygons} /> */}
-      {/* <Marker position={[center.lat, center.lng]}>
-        <Popup>
-          This Marker icon is displayed correctly with <i>leaflet-defaulticon-compatibility</i>.
-        </Popup>
-      </Marker> */}
     </MapContainer>
   );
 }
