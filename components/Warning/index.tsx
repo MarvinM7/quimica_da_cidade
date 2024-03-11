6f42cdc3760ba7a5ed82e19989554927f30048fa
Marvin Miguel <marvin.silva@n3urons.com>
2024-03-11 17:25:51 -0300

    feat: troca do vídeo pelo gif

--- components/Warning/index.tsx
+++ components/Warning/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { League_Gothic } from 'next/font/google';
 
 import styles from './Warning.module.css';
@@ -15,10 +17,14 @@ const leagueGhotic = League_Gothic({
 const Warning = ({text, title}: WarningProps) => {
   return (
     <div className={styles.container}>
-      <video className={styles.video} width="320" height="320" controls preload="none">
-        <source src="/videos/video1.mp4" type="video/mp4" />
-        Seu navegador não suporta tag de vídeo.
-      </video>
+      <Image
+        className={styles.gif}
+        src={'/gifs/gif1.gif'}
+        alt={'Ciclista adoecendo'}
+        width={320}
+        height={320}
+        unoptimized
+      />
       <div className={`${leagueGhotic.className} ${styles.title}`}>{title}</div>
       <div className={styles.text}>{text}</div>
     </div>
